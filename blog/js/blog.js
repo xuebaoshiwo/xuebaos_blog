927e1c5f180558d790e32e4568ccce594c3ea60c
xuebao <2287227024@qq.com>
2024-09-05 09:29:34 +0800

    pagination2

--- blog/js/blog.js
+++ blog/js/blog.js
@@ -39,9 +39,15 @@ async function main(){    // 获取传递过来参数
                 let blogs_num = data.blogs_num
 
                 // 计算得到pages总数
-                let pages_num = blogs / pieces_of_page;
+                let pages_num = (blogs_num / pieces_of_page) >> 0;
 
                 let length = post_ids.length
+                // 先清空blog_container
+                let old_blog_pieces = document.getElementsByClassName('blog-piece');
+                for (let ele of old_blog_pieces){
+                    ele.remove();
+                }
+
                 // 添加到前端blog_container
                 for (let i = 0; i < length; i++){
                     let title = titles[i];
@@ -52,10 +58,14 @@ async function main(){    // 获取传递过来参数
                     let coverMainContent = mainContent.slice(0, coverLength); 
                     add_blog(title, coverMainContent, cover_img_path);
                 }
+
+                // 添加分页器
+                addPagenation(page, pages_num)
+
             })
-            .catch(err => {
-                alert(err)
-            })
+            // .catch(err => {
+            //     alert(err)
+            // })
     }
 
     function add_blog(title, coverMainContent, cover_img_path){
@@ -93,9 +103,45 @@ async function main(){    // 获取传递过来参数
     }
 
     // 加入分页器
-    function addPagenation(pages_num){
+    async function addPagenation(page, pages_num){
         let pagenation = document.getElementById('blog-pagenation');
-        pagenation.innerHTML = ''
+        let page_button_list = document.getElementsByClassName('page_button');
+        // 先移除所有page+button
+        for (let i = 0; i < page_button_list.length; i++){
+            page_button_list[i].remove();
+        }
+        // 当前页面所在页码
+        let standard_page_button;
+        // 再加入新的page_button
+        alert(pages_num)
+        // 获得page_button_container
+        let page_button_container = document.getElementById('page_button_container');
+
+        for (let i = 0; i < pages_num; i++){
+            // create一个新的page_button
+            let page_button = document.createElement('span');
+            page_button.classList.add('page_button');
+            page_button.innerHTML = i + 1;
+            if (i + 1 === page){
+                standard_page_button = page_button;
+            }
+
+            // 绑定event
+            page_button.addEventListener('click', async (event, i) => {
+                // d递归调用getBlogAndShow
+                getBlogAndShow(i + 1, 6);
+            })
+
+            // 加入到page_button_container中
+            page_button_container.appendChild(page_button);
+
+
+        }
+        // 置顶到当前页面
+        // standard_page_button.scrollIntoView({
+        //     behavior: 'smooth' // 平滑滚动
+        // });
+        
     }
 
 }
